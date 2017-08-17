566b8240a78d0e6095e37d4cf0a182cc03b531c6
afeiship <1290657123@qq.com>
2017-08-17 13:15:19 +0800

    issue: add url

--- src/next-date.js
+++ src/next-date.js
@@ -7,7 +7,9 @@
    * dateStr.replace(/\s/g,'T').replace(/\//g,'-');
    * http://www.cnblogs.com/Fooo/p/5284421.html
    * http://blog.csdn.net/sinat_31257007/article/details/49208789
-   *
+   * @thanks to:
+   * http://blog.stevenlevithan.com/archives/date-time-format
+   * https://github.com/felixge/node-dateformat
    * bug:
    * safari invalid date
    */
