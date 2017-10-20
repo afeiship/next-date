f7b63c4f8a08afedc881fab43b2caf07ac270436
afeiship <1290657123@qq.com>
2017-10-20 14:23:35 +0800

    isseu: timeleft cut

--- dist/next-date.js
+++ dist/next-date.js
@@ -9,10 +9,6 @@
   var DATE_DASH = '/';
   var DATE_SPACE = ' ';
   var STRING = 'string';
-  var ONE_DAY = 1000 * 60 * 60 * 24;
-  var ONE_MINUTE = 1000 * 60;
-  var DEFAULT_FORMAT = 'yyyy-mm-dd HH:MM:ss';
-
 
 
   /**
@@ -30,26 +26,6 @@
     statics:{
       now: function(){
         return Date.now() || +(new Date());
-      },
-      parse: function(inTimestamp){
-        var years = Math.floor(inTimestamp / ONE_DAY / 365 );
-        var months = Math.floor(inTimestamp / ONE_DAY / 30 );
-        var weeks = Math.floor(inTimestamp / ONE_DAY / 7);
-        var days = Math.floor(inTimestamp / ONE_DAY);
-        var hours = parseInt(inTimestamp / ONE_MINUTE / 60 % 24 , 10);
-        var minutes = parseInt(inTimestamp / ONE_MINUTE % 60, 10);
-        var seconds = parseInt(inTimestamp / 1000 % 60, 10);
-        var micros = parseInt(inTimestamp % 10,10);
-        return {
-          year: years,
-          month: months,
-          week: weeks,
-          day: days,
-          hour: hours,
-          minute: minutes,
-          second: seconds,
-          micro: micros
-        };
       },
       create: function(inTarget){
         switch(true){
